989e068a2ce7566552e5e46e10be006a5e3b5edc
Daniel Piñeros <odpinerosh@gmail.com>
2021-06-24 20:03:03 -0500

    finished project with React

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -5,12 +5,12 @@ function ComingSoon() {
         <div className="coming-soon">
            <div className="coming-soon-txt">
                 <h1 className="coming-soon-h1">WE'RE <span>COMING SOON</span></h1>
-                    <p className="coming-soon-p">
+                <p className="coming-soon-p">
                     Hello fellow shoppers! We're currently 
                     building our new fashion store. Add your
                     email below to stay up-to-date with 
                     announcements and our launch deals.
-                    </p>
+                </p>
            </div>
         </div>
     );
--- src/components/EmailField.js
+++ src/components/EmailField.js
@@ -1,16 +1,70 @@
-import './EmailField.css';
+import React, { useState } from "react";
+import "./EmailField.css";
+import IconArrow from '../images/icon-arrow.svg';
+import IconError from '../images/icon-error.svg'
 
+const EmailField = () => {
+  
+  const [enteredMail, setEnteredMail] = useState("");
 
-function EmailField() {
-    /*const txtMail = document.getElementById('txtMail');
-    const btnSubmit = document.getElementById('btnSubmit');*/
+  const funValidar = (el) => {
+    if (el === "" || !funTestMail(el)) {
+      console.log("funValidar dice False");
+      return false;
+    } else return true;
+  };
 
-    return (
-        <div className="form-div">
-            <input type="email" name="e-mail" id="txtMail" aria-label="Email Address" placeholder="Email Address" className="input-control"></input>
-            <button name="favorito" type="button" className="button-control" id="btnSubmit">&gt;</button>
-        </div>
-    );
-}
+  const funTestMail = (mail) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail);
+  };
+
+  const clickHandler = (ev) => {
+    const txtMail = document.getElementById("mail-form");
+    const txtInfError = document.getElementById("inf-error");
+    const imgMailError = document.getElementById("btn-error");
+
+    if(!funValidar(enteredMail)) {
+      txtMail.classList.add("input-control-err");
+      txtInfError.classList.add("mail-error-visible");
+      imgMailError.classList.add("mail-error-visible");
+    } else {
+      txtMail.classList.remove("input-control-err");
+      txtInfError.classList.remove("mail-error-visible");
+      imgMailError.classList.remove("mail-error-visible");
+    }
+  };
+
+  const textChangeHandler = (ev) => {
+    ev.target.classList.remove("input-control-err");
+    setEnteredMail(ev.target.value);
+  };
+
+  return (
+    <div className="form-div">
+        <form action="" className="form-elements" id="mail-form">
+          <input
+            onChange={textChangeHandler}
+            type="email"
+            name="e-mail"
+            id="txtMail"
+            aria-label="Email Address"
+            placeholder="Email here"
+            className="input-control"
+          ></input>
+          <img className="btn-error" id="btn-error" src={IconError} alt=""></img>
+          <button
+            onClick={clickHandler}
+            name="favorito"
+            type="button"
+            className="button-control"
+            id="btnSubmit"
+          >
+            <img className="btn-img" src={IconArrow} alt=""></img>
+          </button>
+        </form>
+        <p className="mail-error" id="inf-error">Please provide a valid email</p>          
+    </div>
+  );
+};
 
 export default EmailField;
